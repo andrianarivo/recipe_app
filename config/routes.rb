fa57e5c43e86af76597fbe982270b8e506f28465
andrianarivo <andrianarivo.david@gmail.com>
2023-09-05 18:09:04 +0300

    Update Rails Config

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "recipes#index"
+  root "recipes#all"
   get '/shoppings', to: 'shoppings#index', as: 'shoppings'
+  get '/all', to: 'recipes#all', as: 'public_recipes'
 end
