216eeaf6f7108c122f12c715472dc66aea104649
andrianarivo <andrianarivo.david@gmail.com>
2023-09-05 22:19:06 +0300

    Fix conflict in routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: 'home#index'
   get '/shoppings', to: 'shoppings#index', as: 'shoppings'
-  get '/all', to: 'recipes#all', as: 'public_recipes'
   delete '/ingredients/:id', to: 'recipes#destroy_ingredient', as: 'destroy_ingredient'
 end
