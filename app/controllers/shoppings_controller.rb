49b6ff6f2e970e772a804befd1d5468f874094ce
andrianarivo <andrianarivo.david@gmail.com>
2023-09-06 00:21:22 +0300

    Implement Shopping feature

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -1,5 +1,14 @@
 class ShoppingsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     # list all the shopping lists
+    @recipe = Recipe.find(params[:id])
+    @foods = current_user.foods
+    @ingredients = @recipe.foods
+    @missing_ingredients = @ingredients.select do |ingredient|
+      idx = @foods.find_index(ingredient)
+      @foods[idx].quantity > ingredient.quantity
+    end
   end
 end
