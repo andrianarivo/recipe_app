f0776fc0e01ca5662f11b25ec92e20b1aff957c7
andrianarivo <andrianarivo.david@gmail.com>
2023-09-05 16:51:56 +0300

    Add ability file

---
+++ app/models/ability.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    can :read, :all
+    can :manage, Recipe, { author: user }
+  end
+end
